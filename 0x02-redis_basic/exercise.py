d31ebdbe585fe39d86faffbdff4e9eea0fd48127
Yoseph Tapano <yosephman76@gmail.com>
2023-02-20 12:02:48 +0300

    updated

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -76,6 +76,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''
             Store data in the cache.
